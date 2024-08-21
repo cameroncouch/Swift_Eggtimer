c8eb5314474287afe89e14ba5073a6c312d7da14
Cam Couch <cameronmcouch@gmail.com>
2024-08-21 08:25:12 -0400

    initial commit. printing cook time of eggs

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    let soft = 5;
+    let medium = 8;
+    let hard = 12;
+
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        print(sender.titleLabel?.text ?? "No Egg")
+        var cookTime = 0
+        if (sender.currentTitle == "Soft") {
+            cookTime = soft
+        } 
+        else if (sender.currentTitle == "Medium") {
+            cookTime = medium
+        } 
+        else {
+            cookTime = hard
+        }
+        print("Cook time is \(cookTime) mins")
+    }
     
-
-
 }
